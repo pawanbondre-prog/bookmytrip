e0be440e964c5e2387ee37bdd275093313afeae4
Pawan Bondre <pawan.bondre@gmail.com>
2026-03-11 22:04:03 +0530

    coderefactor

---
+++ src/main/java/com/example/boomytrip/BoomytripApplication.java
@@ -0,0 +1,13 @@
+package com.example.boomytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boomytrip/BoomytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boomytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
